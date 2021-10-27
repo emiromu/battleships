d5e7b0d8786bc05c33390ef212f7ddc7e5333889
Emilien Romulus <emilien.romulus@tuta.io>
2021-10-27 13:04:22 +1100

    out of bounds exception thrown for createBattleship()

--- engine.js
+++ engine.js
@@ -1,21 +1,25 @@
-function createShip(type) {
-    /**Your ‘ships’ will be objects that include their length, 
-     * where they’ve been hit 
-     * and whether or not they’ve been sunk. 
-     * 
-     * 
+function createShip(type, position, orientation) {
+    /**TYPES
     1	Carrier	5
     2	Battleship	4
     3	Cruiser	3
     4	Submarine	3
     5	Destroyer	2
+
+    position is the coordinates (x,y) of the first block
+    orientation is either horizontal (x) or vertical (y), and only goes (left to right, up to down)
+    assume a 10x10 grid, (0,0) in the top left corner
     */
+
     let length = 0;
-    let isHit = false;
+    //bocks is an array representing each part of the ship
+    //each element (or 'ship block') is an object made of coordinates (x,y) and a isHit boolean
+    let blocks = [];
+    //Todo, functions to check ship integrity (check blocks) and updated isSunk
     let isSunk = false;
 
     switch(type){
-        case 'Carrier': length = 5;
+        case 'Carrier': length = 5; 
             break;
         case 'Battleship': length = 4;
             break;
@@ -27,7 +31,22 @@ function createShip(type) {
             break;
     };
 
+    for(let i=0; i<length; i++){
+        /**position + orientation validity test */
+        if(orientation == 'x' && (position.x + i >10)){
+            //x overflow
+            throw new Error('Ship out of x-bounds');
+            return;
+        }
+        if(orientation == 'y' && (position.y + i >10)){
+            //y overflow
+            //throw 'Ship out of y-bounds';
+            throw new Error('Ship out of y-bounds');
+            return;
+        }
+        blocks[i]={x: position.x+i, y:position.y+i, isHit: false}
+    };
 
-    return {type, length, isHit, isSunk};
+    return {type, length, blocks, isSunk};
     };
 exports.createShip = createShip;
--- engine.test.js
+++ engine.test.js
@@ -1,26 +1,16 @@
 const engine = require('./engine');
 
-  test('Creating a carrier (length 5, not hit, not sunk)', () => {
-    expect(engine.createShip('Carrier'))
-    .toEqual({type:'Carrier', length:5, isHit:false, isSunk:false});
+  test('Creating a carrier (length 5) at position 0,0 with x-orientation', () => {
+    expect(engine.createShip('Carrier',{x:0,y:0},'x'))
+    .toEqual({
+    type:'Carrier',
+    length:5,
+    blocks: [{isHit:false,x:0,y:0},{isHit:false,x:1,y:1},{isHit:false,x:2,y:2},{isHit:false,x:3,y:3},{isHit:false,x:4,y:4}],
+    isSunk:false
+    });
   });
 
-  test('Creating a battleship (length 4, not hit, not sunk)', () => {
-    expect(engine.createShip('Battleship'))
-    .toEqual({type:'Battleship', length:4, isHit:false, isSunk:false});
-  });
-
-  test('Creating a cruiser (length 3, not hit, not sunk)', () => {
-    expect(engine.createShip('Cruiser'))
-    .toEqual({type:'Cruiser', length:3, isHit:false, isSunk:false});
-  });
-
-  test('Creating a submarine (length 3, not hit, not sunk)', () => {
-    expect(engine.createShip('Submarine'))
-    .toEqual({type:'Submarine', length:3, isHit:false, isSunk:false});
-  });
-
-  test('Creating a destroyer (length 2, not hit, not sunk)', () => {
-    expect(engine.createShip('Destroyer'))
-    .toEqual({type:'Destroyer', length:2, isHit:false, isSunk:false});
+  test('Creating a battleship (length 4) at position 9,9 with y-orientation', () => {
+    expect(() => engine.createShip('Battleship',{x:9,y:9},'y'))
+    .toThrow('Ship out of y-bounds');
   });
